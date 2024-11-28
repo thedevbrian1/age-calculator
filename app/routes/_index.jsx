354b4f32cefe765731d275e5f6a00efc9a5cfe58
thedevbrian1 <brayomwas95@gmail.com>
2024-11-28 11:25:56 +0300

    Do not show invalid results

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -9,27 +9,46 @@ export const meta = () => {
 
 export default function Index() {
   // Declare state variables for storing the form inputs
-  let [day, setDay] = useState(0);
-  let [month, setMonth] = useState(0);
-  let [year, setYear] = useState(0);
+  let [day, setDay] = useState("");
+  let [month, setMonth] = useState("");
+  let [year, setYear] = useState("");
 
   // Declare state variables for storing the date differences
   let [dateDifference, setDateDifference] = useState(0);
   let [monthDifference, setMonthDifference] = useState(0);
   let [yearDifference, setYearDifference] = useState(0);
 
+  // Declare state variables for errors
+  let [dayError, setDayError] = useState(false);
+  let [monthError, setMonthError] = useState(false);
+  let [yearError, setYearError] = useState(false);
+
   function handleClick() {
     let today = new Date();
-    console.log({ today });
 
     let currentDay = today.getDate();
-    console.log({ currentDay });
 
     let currentMonth = today.getMonth();
     console.log({ currentMonth });
 
     let currentYear = today.getFullYear();
     console.log({ currentYear });
+
+    if (!day || Number(day) < 1 || Number(day) > 31) {
+      setDayError(true);
+    } else {
+      setDayError(false);
+    }
+    if (!month || Number(month) < 0 || Number(month) > 11) {
+      setMonthError(true);
+    } else {
+      setMonthError(false);
+    }
+    if (!year || Number(year) > currentYear) {
+      setYearError(true);
+    } else {
+      setYearError(false);
+    }
 
     setDateDifference(currentDay - day);
     setMonthDifference(currentMonth - month);
@@ -44,16 +63,41 @@ export default function Index() {
     <main className="max-w-4xl mx-auto">
       <div className="flex gap-4 mt-28">
         <div>
-          <Label htmlFor="day">Day</Label>
-          <Input id="day" value={day} setValue={setDay} />
+          <Label htmlFor="day" hasError={dayError}>
+            Day
+          </Label>
+          <Input id="day" value={day} setValue={setDay} hasError={dayError} />
+          {dayError ? (
+            <p className="text-red-500">Must be a valid day</p>
+          ) : null}
         </div>
         <div>
-          <Label htmlFor="month">Month</Label>
-          <Input id="month" value={month} setValue={setMonth} />
+          <Label htmlFor="month" hasError={monthError}>
+            Month
+          </Label>
+          <Input
+            id="month"
+            value={month}
+            setValue={setMonth}
+            hasError={monthError}
+          />
+          {monthError ? (
+            <p className="text-red-500">Must be a valid month</p>
+          ) : null}
         </div>
         <div>
-          <Label htmlFor="year">Year</Label>
-          <Input id="year" value={year} setValue={setYear} />
+          <Label htmlFor="year" hasError={yearError}>
+            Year
+          </Label>
+          <Input
+            id="year"
+            value={year}
+            setValue={setYear}
+            hasError={yearError}
+          />
+          {yearError ? (
+            <p className="text-red-500">Must be less than current year</p>
+          ) : null}
         </div>
       </div>
       <div className="flex justify-end mt-8 pr-4">
@@ -66,23 +110,38 @@ export default function Index() {
         </button>
       </div>
       <div className="mt-8">
-        <Display numValue={yearDifference} textValue={"years"} />
-        <Display numValue={monthDifference} textValue={"months"} />
-        <Display numValue={dateDifference} textValue={"days"} />
+        <Display
+          numValue={yearDifference}
+          textValue={"years"}
+          hasError={yearError}
+        />
+        <Display
+          numValue={monthDifference}
+          textValue={"months"}
+          hasError={monthError}
+        />
+        <Display
+          numValue={dateDifference}
+          textValue={"days"}
+          hasError={dayError}
+        />
       </div>
     </main>
   );
 }
 
-function Label({ children, htmlFor }) {
+function Label({ children, htmlFor, hasError }) {
   return (
-    <label htmlFor={htmlFor} className="uppercase">
+    <label
+      htmlFor={htmlFor}
+      className={`uppercase ${hasError ? "text-red-500" : ""}`}
+    >
       {children}
     </label>
   );
 }
 
-function Input({ id, value, setValue }) {
+function Input({ id, value, setValue, hasError }) {
   return (
     <input
       type="number"
@@ -91,15 +150,18 @@ function Input({ id, value, setValue }) {
       onChange={(event) => {
         setValue(event.target.value);
       }}
-      className="px-4 py-2 rounded-md block border border-gray-300"
+      className={`px-4 py-2 rounded-md block border ${
+        hasError ? "border-red-500" : "border-gray-300"
+      } `}
     />
   );
 }
 
-function Display({ numValue, textValue }) {
+function Display({ numValue, textValue, hasError }) {
   return (
     <p className="font-bold text-7xl">
-      <span className="text-purple-500">{numValue}</span> {textValue}
+      <span className="text-purple-500">{hasError ? "--" : numValue}</span>{" "}
+      {textValue}
     </p>
   );
 }
